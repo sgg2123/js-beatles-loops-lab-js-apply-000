fffc4e9f4d438fc62f9d70cc6ee5aa707eb04370
sgg2123 <sgg2123@columbia.edu>
2018-04-08 18:05:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-```var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Starr"];
-var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];```
-
 function theBeatlesPlay(musicians, instruments) {
   var emptyArray = [];
   var counter;
