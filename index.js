804f3e93c9f91bd1823bd2b35cc4473d824455b7
sgg2123 <sgg2123@columbia.edu>
2018-04-08 18:39:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,14 +5,13 @@ function theBeatlesPlay(musicians, instruments) {
     var newString = musicians[counter] + " plays " + instruments[counter];
     newArray.push(newString);
   }
-
   return newArray
 }
 
 function johnLennonFacts(facts) {
   var newArray = [];
   var counter = 0;
-  while (counter<facts.length) {
+  while (counter < facts.length) {
     var loudfact = facts[counter] + "!!!"
     newArray.push(loudfact);
     counter++;
